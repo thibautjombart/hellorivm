2b75ed3564a22f57b9d77a694b5d153a22a0f7e5
Thibaut <thibautjombart@gmail.com>
2015-11-03 11:32:21 +0100

    Adding files

---
+++ R/goodbye.R
@@ -0,0 +1,14 @@
+#' Goodbye RIVM
+#'
+#' This is what the function does...
+#'
+#' @param x this is a chracter string
+#'
+#' @export
+#'
+goodbye.rivm <- function(x="RIVM"){
+    cat("Goodbye",x,"!\n")
+
+    ## RETURN ##
+    return(invisible(NULL))
+} #
---
+++ R/hello.R
@@ -0,0 +1,13 @@
+
+#' Hello RIVM
+#'
+#' This is what the function does...
+#'
+#' @param x this is a chracter string
+#'
+#' @export
+#'
+hello.rivm <- function(x="RIVM"){
+    cat("Hello",x,"!\n")
+    return(invisible(NULL))
+}
