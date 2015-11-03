907e2e7f85cf8f4196f69471462c471ecad576b0
Thibaut <thibautjombart@gmail.com>
2015-11-03 11:38:53 +0100

    added a new function

--- R/hello.R
+++ R/hello.R
@@ -11,3 +11,17 @@ hello.rivm <- function(x="RIVM"){
     cat("Hello",x,"!\n")
     return(invisible(NULL))
 }
+
+
+
+#' This is coffee time
+#'
+#' Hell yeah!!
+#'
+#' @export
+#'
+coffee.time <- function(){
+    current.time <- Sys.time()
+    out <- current.time + round(runif(1, min=-60, max=60))
+    return(out)
+}
